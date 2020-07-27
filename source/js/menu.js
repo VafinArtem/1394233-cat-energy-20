868e71ba224e5ae49ce3d5fcda79f0cd8eae03fa
Vafin Artem <a.vafin85@gmail.com>
2020-07-27 13:42:19 +0500

    подключил скрипты на картк и открытие закрытие меню

---
+++ source/js/menu.js
@@ -0,0 +1,14 @@
+let menuButton = document.querySelector(".js-button")
+let menu = document.querySelector(".js-menu")
+
+menuButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (menuButton.classList.contains("main-nav__toggle--active")) {
+    menu.classList.remove("main-nav__list--active");
+    menuButton.classList.remove("main-nav__toggle--active");
+  } else {
+    menu.classList.add("main-nav__list--active");
+    menuButton.classList.add("main-nav__toggle--active");
+  };
+}
+)
