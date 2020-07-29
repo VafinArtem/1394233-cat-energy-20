32c9eac147409bccc5d875154422da006124db2d
Vafin Artem <a.vafin85@gmail.com>
2020-07-29 18:01:26 +0500

    Настроил работу мобильного меню при отключенном JS

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,9 @@
 let menuButton = document.querySelector(".js-button")
 let menu = document.querySelector(".js-menu")
 
+menuButton.classList.remove("main-nav__toggle--no-js");
+menu.classList.remove("main-nav__list--no-js")
+
 menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (menuButton.classList.contains("main-nav__toggle--active")) {
