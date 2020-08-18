e25d2fb7b79dea36840e26062e3d96272733a267
Vafin Artem <a.vafin85@gmail.com>
2020-08-18 20:37:43 +0500

    Поправил общий файл скрипта + убрал активность мобильного менб по умолчанию

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,21 @@
+// Menu
+
+let menuButton = document.querySelector(".js-button");
+let menu = document.querySelector(".js-menu");
+
+menuButton.classList.remove("main-nav__toggle--no-js");
+menu.classList.remove("main-nav__list--no-js");
+menuButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (menuButton.classList.contains("main-nav__toggle--active")) {
+    menu.classList.remove("main-nav__list--active");
+    menuButton.classList.remove("main-nav__toggle--active");
+  } else {
+    menu.classList.add("main-nav__list--active");
+    menuButton.classList.add("main-nav__toggle--active");
+  };
+});
+
 // Form
 
 let form = document.querySelector(".form")
@@ -18,25 +36,6 @@ form.addEventListener("submit", function(evt) {
       formInputs[i].offsetWidth = formInputs[i].offsetWidth;
       formInputs[i].classList.add("form__input--invalid");
     };
-  };
-});
-
-
-// Menu
-
-let menuButton = document.querySelector(".js-button");
-let menu = document.querySelector(".js-menu");
-
-menuButton.classList.remove("main-nav__toggle--no-js");
-menu.classList.remove("main-nav__list--no-js");
-menuButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (menuButton.classList.contains("main-nav__toggle--active")) {
-    menu.classList.remove("main-nav__list--active");
-    menuButton.classList.remove("main-nav__toggle--active");
-  } else {
-    menu.classList.add("main-nav__list--active");
-    menuButton.classList.add("main-nav__toggle--active");
   };
 });
 
